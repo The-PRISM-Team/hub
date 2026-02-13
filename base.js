1b66022daf4b10b02ffde059dc509c3831db71e6
Soph B (ccjt) <148160919+ccjit@users.noreply.github.com>
2026-02-13 20:15:17 -0300

    i am so fu

--- base.js
+++ base.js
@@ -1,11 +1,17 @@
 if (localStorage.darkMode == null) localStorage.darkMode = 'false';
 
+document.body.className =
+    localStorage.darkMode === 'true' ?
+    'dark'
+    :
+    'light';
+
 function toggleDark() {
+    localStorage.darkMode = localStorage.darkMode !== 'true';
+
     document.body.className =
         localStorage.darkMode === 'true' ?
         'dark'
         :
         'light';
 }
-
-toggleDark()
