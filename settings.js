5ee64cef24188196bafddb36fc81656c3e74b1b6
Soph B (ccjt) <148160919+ccjit@users.noreply.github.com>
2026-02-13 21:30:55 -0300

    Oops

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const darkmodeCheckbox = document.getElementById('darkmode-checkbox');
 darkmodeCheckbox.checked = localStorage.darkMode === 'true';
 darkmodeCheckbox.addEventListener('change', ()=>{
-    toggleDark(darkmodeCheckbox);
+    toggleDark(localStorage.darkMode !== 'true');
 })
